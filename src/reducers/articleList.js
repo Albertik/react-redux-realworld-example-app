cf10f046ccdf496d49360ccce78d060466cb7188
Albert <kovalevskij.albert@gmail.com>
2017-11-12 19:10:52 +0200

    changes

--- src/reducers/articleList.js
+++ src/reducers/articleList.js
@@ -50,9 +50,9 @@ export default (state = {}, action) => {
       return {
         ...state,
         pager: action.pager,
-        tags: action.payload[0].tags,
-        articles: action.payload[1].articles,
-        articlesCount: action.payload[1].articlesCount,
+        tags: action.payload[0] && action.payload[0].tags,
+        articles: action.payload[1] && action.payload[0].articles,
+        articlesCount: action.payload[1] && action.payload[0].articlesCount,
         currentPage: 0,
         tab: action.tab
       };
--- src/reducers/common.js
+++ src/reducers/common.js
@@ -18,7 +18,7 @@ import {
 } from '../constants/actionTypes';
 
 const defaultState = {
-  appName: 'Conduit',
+  appName: 'Verdene',
   token: null,
   viewChangeCounter: 0
 };
